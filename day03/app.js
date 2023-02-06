d183a0a8f548099b082103d6d73ccb87d033a20c
Boon Juan <boonjuan1@gmail.com>
2023-02-06 20:36:26 +0800

    Completed, Half Mobile friendly

---
+++ day03/app.js
@@ -0,0 +1,8 @@
+const initApp = () => {
+  const hamburger = document.getElementById("hamburger");
+  const menu = document.getElementById("menu");
+
+  hamburger.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+  });
+};
