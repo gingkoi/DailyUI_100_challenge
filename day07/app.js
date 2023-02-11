92aa125dab74e52e617b2d4aa64c7517db52f2f9
Boon Juan <boonjuan1@gmail.com>
2023-02-11 22:11:54 +0800

    shitty js code

--- day07/app.js
+++ day07/app.js
@@ -1,12 +1,18 @@
-const account_setting = document.querySelector("#account_click");
-const show_account = document.querySelector("#account");
-const noti_setting = document.querySelector("#noti_click");
-const show_noti = document.querySelector("#noti");
+const tab_account = document.querySelector("#tab-account");
+const tab_noti = document.querySelector("#tab-noti");
 
-account_setting.addEventListener("click", () => {
-  show_account.classList.toggle("hidden");
+const date = new Date();
+console.log(date);
+
+tab_noti.addEventListener("click", () => {
+  document.querySelector("#account").classList.add("hidden");
+  document.querySelector("#noti").classList.remove("hidden");
+  document.querySelector("#tab-noti").classList.add("bg-[#f7faff]");
+  document.querySelector("#tab-account").classList.remove("bg-[#f7faff]");
 });
-
-noti_setting.addEventListener("click", () => {
-  show_noti.classList.toggle("hidden");
+tab_account.addEventListener("click", () => {
+  document.querySelector("#noti").classList.add("hidden");
+  document.querySelector("#account").classList.remove("hidden");
+  document.querySelector("#tab-account").classList.add("bg-[#f7faff]");
+  document.querySelector("#tab-noti").classList.remove("bg-[#f7faff]");
 });
