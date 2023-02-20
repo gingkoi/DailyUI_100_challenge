cc6227292bc2a0c09f5d15481dad98a952672a67
Boon Juan <boonjuan1@gmail.com>
2023-02-20 18:27:04 +0800

    50% done, only left with js

---
+++ day14/app.js
@@ -0,0 +1,5 @@
+let calculateBtn = document.querySelector("#calculateBtn");
+calculateBtn.addEventListener("click", () => {
+  let person1 = document.querySelector("#person1").value.toUpperCase();
+  let person2 = document.querySelector("#person2").value.toUpperCase();
+});
