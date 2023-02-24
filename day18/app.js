6cc44e6f848e0a3127dd1faa2edf5ab3b80055fd
Boon Juan <boonjuan1@gmail.com>
2023-02-24 15:56:14 +0800

    Added animation

--- day18/app.js
+++ day18/app.js
@@ -1,9 +1,17 @@
 const price_pointer = document.querySelector("#price-pointer");
 const price_detail = document.querySelector("#price-details");
+const price_pointer_2 = document.querySelector("#price-pointer-2");
+const price_detail_2 = document.querySelector("#price-details-2");
 
 price_pointer.addEventListener("mouseover", () => {
   price_detail.classList.toggle("hidden");
 });
 price_pointer.addEventListener("mouseout", () => {
   price_detail.classList.toggle("hidden");
+});
+price_pointer_2.addEventListener("mouseover", () => {
+  price_detail_2.classList.toggle("hidden");
+});
+price_pointer_2.addEventListener("mouseout", () => {
+  price_detail_2.classList.toggle("hidden");
 });
